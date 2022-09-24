7b7080c5a4d1dafb474149a9ad36d240429e6296
Ilya <ilya.ilyin5@gmail.com>
2022-09-24 14:27:09 +0300

    add slug to Product model

--- demoshop/products/admin.py
+++ demoshop/products/admin.py
@@ -2,21 +2,20 @@ from django.contrib import admin
 # Register your models here.
 from treebeard.admin import TreeAdmin
 from treebeard.forms import movenodeform_factory
-
 from .models import Folder, Product
 
 
+@admin.register(Folder)
 class FolderAdmin(TreeAdmin):
     form = movenodeform_factory(Folder)
 
 
-admin.site.register(Folder, FolderAdmin)
+# admin.site.register(Folder, FolderAdmin)
 
-
+@admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['folder', 'name', 'price']
     list_filter = ['folder']
     list_editable = ['name', 'price']
 
-
-admin.site.register(Product, ProductAdmin)
+# admin.site.register(Product, ProductAdmin)
--- demoshop/products/models.py
+++ demoshop/products/models.py
@@ -12,11 +12,9 @@ class Folder(MP_Node):
     node_order_by = ['name']
 
     def save(self, *args, **kwargs):
-        self.slug = slugify(f'{self.id}_{self.name}')
+        self.slug = slugify(f'folder-{self.id}')
         super(Folder, self).save(*args, **kwargs)
 
-    # def __str__(self):
-    #     return 'Folder: {}'.format(self.name)
     def __str__(self):
         return self.name
 
@@ -25,5 +23,13 @@ class Product(models.Model):
     folder = models.ForeignKey(Folder, on_delete=models.CASCADE)
     # tarif_item_id = models.IntegerField(unique=True, null=True, blank=True)  # TarifItemID Servio
     name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True, unique=True, blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(f'product-{self.id}')
+        super(Product, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
