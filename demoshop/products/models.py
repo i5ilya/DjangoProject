847a1c349f7a8472fdc5f7d52de89c187e743abe
Ilya <ilya.ilyin5@gmail.com>
2022-10-02 19:03:38 +0300

    add line to save model instance for generate id

--- demoshop/products/models.py
+++ demoshop/products/models.py
@@ -18,6 +18,7 @@ class Folder(MP_Node):
 
     def get_absolute_url(self):
         return reverse('folder_list', args=[self.slug])
+
     def __str__(self):
         return self.name
 
@@ -27,13 +28,14 @@ class Product(models.Model):
     # tarif_item_id = models.IntegerField(unique=True, null=True, blank=True)  # TarifItemID Servio
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True, unique=True, blank=True, null=True)
-    image_file = models.ImageField(upload_to='images/', blank=True)
+    image_file = models.ImageField(upload_to='images/', blank=True, default='images/default.jpg')
     price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
 
     def save(self, *args, **kwargs):
+        super(Product, self).save(*args, **kwargs)  # сохраняем, чтобы получить id
         self.slug = slugify(f'product-{self.id}')
-        super(Product, self).save(*args, **kwargs)
+        super(Product, self).save(*args, **kwargs)  # сохраняем после сделанных нами действий.
 
     def get_absolute_url(self):
         return reverse('product_detail', args=[self.slug])
@@ -42,5 +44,6 @@ class Product(models.Model):
     def image_url(self):
         if self.image_file and hasattr(self.image_file, 'url'):
             return self.image_file.url
+
     def __str__(self):
         return self.name
