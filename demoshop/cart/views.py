6b5484a4e3dee6b82b10de6b2723794480e6dd6b
Ilya <ilya.ilyin5@gmail.com>
2022-10-16 16:40:56 +0300

    cart html corrections

--- demoshop/cart/views.py
+++ demoshop/cart/views.py
@@ -18,15 +18,14 @@ def cart_add(request, product_id):
                  update_quantity=cd['update'])
     return redirect('cart:cart_detail')
 
+
 def cart_remove(request, product_id):
     cart = Cart(request)
     product = get_object_or_404(Product, id=product_id)
     cart.remove(product)
     return redirect('cart:cart_detail')
 
+
 def cart_detail(request):
     cart = Cart(request)
     return render(request, 'cart/cart_detail.html', {'cart': cart})
-
-
-
