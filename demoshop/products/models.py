5820076fe4d615fdfa6b6280dce3690da5d4ca33
Ilya <ilya.ilyin5@gmail.com>
2022-11-10 08:39:23 +0200

    back save method

--- demoshop/products/models.py
+++ demoshop/products/models.py
@@ -14,9 +14,10 @@ class Folder(MP_Node):
     slug = models.SlugField(max_length=200, db_index=True, unique=True, blank=True, null=True)
     node_order_by = ['name']
 
-    # def save(self, *args, **kwargs):
-    #     self.slug = slugify(f'folder-{self.id}')
-    #     super(Folder, self).save(*args, **kwargs)
+    def save(self, *args, **kwargs):
+        super(Folder, self).save(*args, **kwargs)
+        self.slug = slugify(f'folder-{self.id}')
+        super(Folder, self).save(*args, **kwargs)
 
     def get_absolute_url(self):
         return reverse('folder_list', args=[self.slug])
@@ -34,10 +35,10 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
 
-    # def save(self, *args, **kwargs):
-    #     super(Product, self).save(*args, **kwargs)  # сохраняем, чтобы получить id
-    #     self.slug = slugify(f'product-{self.id}')
-    #     super(Product, self).save(*args, **kwargs)  # сохраняем после сделанных нами действий.
+    def save(self, *args, **kwargs):
+        super(Product, self).save(*args, **kwargs)  # сохраняем, чтобы получить id
+        self.slug = slugify(f'product-{self.id}')
+        super(Product, self).save(*args, **kwargs)  # сохраняем после сделанных нами действий.
 
     def get_absolute_url(self):
         return reverse('product_detail', args=[self.slug])
@@ -51,15 +52,15 @@ class Product(models.Model):
         return self.name
 
 
-@receiver(post_save, sender=Product)
-def post_save_product(sender, instance, created, **kwargs):
-    if created:
-        instance.slug = slugify(f'product-{instance.id}')
-        instance.save(update_fields=['slug'])
+# @receiver(post_save, sender=Product)
+# def post_save_product(sender, instance, created, **kwargs):
+#     if created:
+#         instance.slug = slugify(f'product-{instance.id}')
+#         instance.save(update_fields=['slug'])
 
 
-@receiver(post_save, sender=Folder)
-def post_save_folder(sender, instance, created, **kwargs):
-    if created:
-        instance.slug = slugify(f'folder-{instance.id}')
-        instance.save(update_fields=['slug'])
+# @receiver(post_save, sender=Folder)
+# def post_save_folder(sender, instance, created, **kwargs):
+#     if created:
+#         instance.slug = slugify(f'folder-{instance.id}')
+#         instance.save(update_fields=['slug'])
