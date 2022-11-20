5c59d94338d6d1aac52e9c9582257b003a12c917
Ilya <ilya.ilyin5@gmail.com>
2022-11-20 18:41:17 +0200

    js function to add to cart

---
+++ demoshop/static/add_and_update_cart.js
@@ -0,0 +1,34 @@
+function add_and_update_cart(item_id) {
+
+    let product_id = item_id;
+    //const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
+    //var csrftoken = getCsrf();
+    const csrftoken = csrf;
+    fetch('/cart/add_js', {
+      headers: {
+          'Content-Type': 'application/json',
+          //'X-CSRFToken': '{{ csrf_token }}',
+          'X-CSRFToken': csrftoken,
+
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        //csrfmiddlewaretoken: '{{ csrf_token }}',  
+        "product_id": product_id,
+        //action : 'post'
+      })
+  }).then(function (response) {
+      if (response.ok) {
+          response.json().then(function (response) {
+              //console.log(response.cart_len);
+              document.getElementById('cart_len').innerHTML = response.cart_len;
+              document.getElementById('cart_sum').innerHTML = response.cart_sum;
+          });
+      } else {
+          throw Error('Something went wrong');
+      }
+  }).catch(function (error) {
+      console.log(error);
+  });
+ }
+
